b4421115508d057948d2844887532b3e6500be6c
김재환 <woghks7859@gmail.com>
2023-05-19 17:05:09 +0900

    FEAT: Done execute creating post

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './board.model';
 
@@ -8,8 +8,16 @@ export class BoardsController {
   constructor(private boardsService: BoardsService) {}
 
   // 메서드와 함께 getter 를 선언해야한다.
-  @Get('/')
+  @Get('/') // R
   getAllBoard(): Board[] {
     return this.boardsService.getAllBoards();
+  }
+
+  @Post() // C
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Board {
+    return this.boardsService.createBoard(title, description);
   }
 }
