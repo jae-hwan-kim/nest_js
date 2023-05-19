c07d89fbd724e155cd859d2819ba9d85e10750ee
김재환 <woghks7859@gmail.com>
2023-05-19 14:38:48 +0900

    FEAT: create Read of CRUD

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { BoardsService } from './boards.service';
 
-@Controller('boards')
-export class BoardsController {}
+@Controller('boards') // location
+export class BoardsController {
+  // Service injection
+  constructor(private boardsService: BoardsService) {}
+
+  // 메서드와 함께 getter 를 선언해야한다.
+  @Get('/')
+  getAllBoard() {
+    return this.boardsService.getAllBoards();
+  }
+}
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  // 다른 곳에서 참조하는 것을 방지하기 위해 private 으로 선언
+  private boards = ['a'];
+
+  getAllBoards() {
+    return this.boards;
+  }
+}
