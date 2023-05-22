d6689d7a207127ded7d1bcab7fbb22be60880f39
김재환 <woghks7859@gmail.com>
2023-05-22 18:24:09 +0900

    FEAT: create custom pipe & exception

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -14,6 +14,7 @@ import {
 import { BoardsService } from './boards.service';
 import { Board, BoardStatus } from './board.model';
 import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 @Controller('boards') // location
 export class BoardsController {
@@ -54,7 +55,7 @@ export class BoardsController {
   @Patch('/:id/status') // U
   updateBoardStatus(
     @Param('id') id: string,
-    @Body('status') status: BoardStatus,
+    @Body('status', BoardStatusValidationPipe) status: BoardStatus,
   ) {
     return this.boardsService.updateBoardStatus(id, status);
   }
---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,32 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common';
+import { BoardStatus } from '../board.model';
+
+export class BoardStatusValidationPipe implements PipeTransform {
+  readonly StatusOptions = [BoardStatus.PRIVATE, BoardStatus.PUBLIC];
+
+  transform(value: any) {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`${value} isn't in the status`);
+    }
+  }
+
+  private isStatusValid(status: any) {
+    const index = this.StatusOptions.indexOf(status);
+    return index !== -1;
+  }
+  //   transform(value: any, metadata: ArgumentMetadata) {
+  //     console.log('value', value);
+  //     console.log('metadata', metadata);
+  //     if (value !== 'PUBLIC' || value !== 'PRIVATE') {
+  //       throw new NotFoundException('error');
+  //     }
+
+  //     return value;
+  //   }
+}
