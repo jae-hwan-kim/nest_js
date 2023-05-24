b0ea9e58f14f3dd4a66eb5f511e0e28bd475c5de
김재환 <woghks7859@gmail.com>
2023-05-24 18:08:19 +0900

    FEAT: 인증된 유저만 보드 열람할 수 있게 Board 에 UseGuard 추가

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -9,23 +9,21 @@ import {
   ValidationPipe,
   Delete,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './board.entity';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatus } from './board-status.enums';
+import { AuthGuard } from '@nestjs/passport';
 // import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 @Controller('boards') // location
+@UseGuards(AuthGuard())
 export class BoardsController {
   // Service injection
   constructor(private boardsService: BoardsService) {}
 
-  // 메서드와 함께 getter 를 선언해야한다.
-  // @Get('/') // R
-  // getAllBoard(): Board[] {
-  //   return this.boardsService.getAllBoards();
-  // }
   @Post()
   @UsePipes(ValidationPipe)
   createBoard(@Body() CreateBoardDto: CreateBoardDto): Promise<Board> {
@@ -41,30 +39,6 @@ export class BoardsController {
   getBoardById(@Param('id') id: number): Promise<Board> {
     return this.boardsService.getBoardById(id);
   }
-  // // localhost:5000?id=abc 의 경우 @Body 로 못가져온다. @Param 을 사용한다.
-  // // 만약 localhost:5000?id=abc&title=good 와 같이 2개를 받을 때는 다음과 같이 쓴다.
-  // // @Param() params: string[]
-  // @Get('/:id') // R
-  // getBoardById(@Param('id') id: string): Board {
-  //   const found = this.boardsService.getBoardById(id);
-
-  //   if (!found) {
-  //     throw new NotFoundException(`There is no Board id ${id}`);
-  //   }
-  //   return found;
-  // }
-
-  // @Post() // C
-  // @UsePipes(ValidationPipe)
-  // createBoard(@Body() createBoardDto: CreateBoardDto): Board {
-  //   // createBoard(@Body(ParseIntPipe) createBoardDto: CreateBoardDto): Board {
-  //   return this.boardsService.createBoard(createBoardDto);
-  // }
-
-  // @Delete('/:id') // D
-  // deleteBoard(@Param('id') id: string): void {
-  //   this.boardsService.deleteBoard(id);
-  // }
 
   @Delete('/:id')
   deleteBoard(@Param('id', ParseIntPipe) id): Promise<void> {
