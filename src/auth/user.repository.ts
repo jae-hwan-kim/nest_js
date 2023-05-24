1a314a629c743c23a5b0af1d6d2ea58c914452df
김재환 <woghks7859@gmail.com>
2023-05-24 16:46:39 +0900

    FEAT: 비밀번호 암호화 구현

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -6,6 +6,7 @@ import {
   Injectable,
   InternalServerErrorException,
 } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
@@ -16,7 +17,10 @@ export class UserRepository extends Repository<User> {
   async createUser(authCreadentailsDto: AuthCreadentailsDto): Promise<void> {
     const { username, password } = authCreadentailsDto;
 
-    const user = this.create({ username, password });
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
     try {
       await this.save(user);
     } catch (error) {
