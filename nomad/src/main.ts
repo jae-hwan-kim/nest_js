e4ed6735b27533581c22349f219377cf0d35f4d5
김재환 <woghks7859@gmail.com>
2023-05-31 13:29:12 +0900

    FEAT: 글로벌 파이프 옵션 추가

--- nomad/src/main.ts
+++ nomad/src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true, // 정의되지 않은 키가 들어오면 에러
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
